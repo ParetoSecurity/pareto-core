7f22721b9cae0a3caf8b18ea2b8f13b224fd7ac1
Janez Troha <239513+dz0ny@users.noreply.github.com>
2025-03-18 20:28:32 +0100

    feat: Allow older version of compiler (#132)

--- package.nix
+++ package.nix
@@ -6,7 +6,7 @@ pkgs.buildGo124Module rec {
   pname = "paretosecurity";
   version = "${builtins.hashFile "sha256" "${toString ./go.sum}"}";
   src = ./.;
-  vendorHash = "sha256-XK5ma3c84OakVYEVkEropiNU1KaZsdPpiYw0dkTdfdY=";
+  vendorHash = "sha256-IxjJNjF+fp4iLmo/MW5nJOx7HKuFhw7zgWqg/0remhk=";
   subPackages = ["cmd/paretosecurity"];
   doCheck = true;
 }
