4a8835810a291cc4a429e39e8c0fa447598f8430
Janez T <hey@dz0ny.dev>
2025-03-20 17:22:38 +0100

    fix: improve integration test for paretosecurity check command output

--- test/integration/nixos.py
+++ test/integration/nixos.py
@@ -1,9 +1,10 @@
-assert "Pareto Security CLI" in machine.succeed("paretosecurity --help")
 
-res = machine.fail("paretosecurity check")
-end_line = res.count("Checks completed")
-dial_error_count = res.count("Failed to connect to helper")
-assert (
-    dial_error_count == 0
-), f"Helper could not start, found : {dial_error_count} calls to dial error"
-assert end_line > 0, f"End line not found, found : {res}"
+help = machine.succeed("paretosecurity --help")
+assert "Pareto Security CLI" in help
+print(help)
+
+status, res = machine.execute("paretosecurity check")
+print(res)
+
+assert "Failed to connect to helper" in res , f"Helper could not start"
+assert "Checks completed" in res, f"End line not found"
