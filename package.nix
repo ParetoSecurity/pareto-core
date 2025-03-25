a1bb6cf98a0fbacf6d04361d1dc00a4185d9e0c0
Janez T <hey@dz0ny.dev>
2025-03-25 13:45:22 +0100

    chore(deps): update dependencies

--- package.nix
+++ package.nix
@@ -6,6 +6,6 @@ pkgs.buildGo124Module rec {
   pname = "paretosecurity";
   version = "${builtins.hashFile "sha256" "${toString ./go.sum}"}";
   src = ./.;
-  vendorHash = "sha256-APxHPQ+08flAWdgVLi7BAJ4D/8klWzKIF+v7dlC3y28=";
+  vendorHash = "sha256-JJPfCJtbgcoobzT9zpiYpxKou+xXlU4EhYcQbPjpdW8=";
   doCheck = true;
 }
