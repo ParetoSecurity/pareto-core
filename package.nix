967ba149faf65396875f5ec98c7cc034f1091429
Janez T <hey@dz0ny.dev>
2025-03-26 17:07:35 +0100

    feat: add table formatting to PrintStates for improved state display

--- package.nix
+++ package.nix
@@ -6,6 +6,6 @@ pkgs.buildGo124Module rec {
   pname = "paretosecurity";
   version = "${builtins.hashFile "sha256" "${toString ./go.sum}"}";
   src = ./.;
-  vendorHash = "sha256-JJPfCJtbgcoobzT9zpiYpxKou+xXlU4EhYcQbPjpdW8=";
+  vendorHash = "sha256-JuUI450GTL6mbc/k9JsXg4D42dXYqNdZUWtbfJgw2mQ=";
   doCheck = true;
 }
